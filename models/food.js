94a5d97b602b0c8c89c8bf03012aec11516983d2
maxscores <maxstackhouse@gmail.com>
2018-03-25 14:12:03 -0600

    Add food model, all

---
+++ models/food.js
@@ -0,0 +1,11 @@
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
+
+class Food {
+  all() {
+    return database.raw('SELECT * FROM foods')
+  }
+}
+
+module.exports = Food
