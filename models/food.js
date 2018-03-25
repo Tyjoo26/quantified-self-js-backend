e62e3f53c1865cedc2e263203a342e630591d45c
maxscores <maxstackhouse@gmail.com>
2018-03-25 15:42:41 -0600

    Only send id, name, calories

--- models/food.js
+++ models/food.js
@@ -4,21 +4,21 @@ const database = require('knex')(configuration)
 
 class Food {
   all() {
-    return database.raw('SELECT * FROM foods')
+    return database.raw('SELECT id, name, calories FROM foods')
   }
   find(id) {
-    return database.raw('SELECT * FROM foods WHERE id= ?', id)
+    return database.raw('SELECT id, name, calories FROM foods WHERE id= ?', id)
   }
   create(params) {
     return database.raw(`INSERT INTO foods(name, calories)
     VALUES(?, ?)
-    RETURNING *`, [params.name, params.calories])
+    RETURNING id, name, calories`, [params.name, params.calories])
   }
   update(id, params) {
     return database.raw(`UPDATE foods
       SET name = ?, calories = ?
       WHERE id = ?
-      RETURNING *`, [params.name, params.calories, id])
+      RETURNING id, name, calories`, [params.name, params.calories, id])
   }
   destroy(id) {
     return database.raw(`DELETE FROM foods WHERE id= ?`, id)
