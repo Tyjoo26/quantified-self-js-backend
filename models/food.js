099ad617c1bfc5091009fa6f1cbd318a83b3c3bf
maxscores <maxstackhouse@gmail.com>
2018-03-25 19:48:06 -0600

    Reorganize food model

--- models/food.js
+++ models/food.js
@@ -7,21 +7,27 @@ class Food {
     return database.raw('SELECT id, name, calories FROM foods')
   }
   find(id) {
-    return database.raw('SELECT id, name, calories FROM foods WHERE id= ?', id)
+    return database.raw(`SELECT id, name, calories FROM foods
+                         WHERE id= ?`,
+                         id)
   }
   create(params) {
     return database.raw(`INSERT INTO foods(name, calories)
-    VALUES(?, ?)
-    RETURNING id, name, calories`, [params.name, params.calories])
+                         VALUES(?, ?)
+                         RETURNING id, name, calories`,
+                         [params.name, params.calories])
   }
   update(id, params) {
     return database.raw(`UPDATE foods
-      SET name = ?, calories = ?
-      WHERE id = ?
-      RETURNING id, name, calories`, [params.name, params.calories, id])
+                         SET name = ?, calories = ?
+                         WHERE id = ?
+                         RETURNING id, name, calories`,
+                         [params.name, params.calories, id])
   }
   destroy(id) {
-    return database.raw(`DELETE FROM foods WHERE id= ?`, id)
+    return database.raw(`DELETE FROM foods
+                         WHERE id= ?`,
+                         id)
   }
 }
 
