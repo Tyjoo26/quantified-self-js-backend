4d055e0b17f864a403c61729499ed131245c492f
maxscores <maxstackhouse@gmail.com>
2018-03-26 15:42:56 -0600

    Add next to routes

--- controllers/foods-controller.js
+++ controllers/foods-controller.js
@@ -2,27 +2,27 @@ var Food = require('../models/food')
 Food = new Food
 
 class FoodsController {
-  index(req, res) {
+  index(req, res, next) {
     Food.all()
       .then((data) => {res.send(data.rows)})
   }
 
-  show(req, res) {
+  show(req, res, next) {
     Food.find(req.params.id)
       .then((data) => {res.send(data.rows)})
   }
 
-  create(req, res) {
+  create(req, res, next) {
     Food.create(req.body.food)
       .then((data) => {res.send(data.rows[0])})
   }
 
-  update(req, res) {
+  update(req, res, next) {
     Food.update(req.params.id, req.body.food)
       .then((data) => {res.send(data.rows[0])})
   }
 
-  destroy(req, res) {
+  destroy(req, res, next) {
     Food.destroy(req.params.id)
       .then((data) => {res.send(data.rows)})
   }
