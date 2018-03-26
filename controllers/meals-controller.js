6993464f81ab5b0cf9b10e21768d1b0084bdd775
maxscores <maxstackhouse@gmail.com>
2018-03-25 19:47:36 -0600

    Refactor mealsWithFoods to model

--- controllers/meals-controller.js
+++ controllers/meals-controller.js
@@ -1,27 +1,11 @@
 var Meal = require('../models/meal')
 
-let meals = new Meal
-
-
+Meal = new Meal
 
 class MealsController {
   index(req, res) {
-    meals.all()
-    .then((data) => {
-      let rows = data.rows
-      let mealFoods = rows.reduce((result, row) => {
-        if (!result[row.meal_id]) {
-          result[row.meal_id] = {id: row.meal_id, name: row.meal_name, foods: []}
-        }
-        result[row.meal_id].foods.push({id: row.food_id, name: row.food_name, calories: row.food_calories})
-        return result
-      }, {})
-      let mealOutput = []
-      for (let meal in mealFoods) {
-        mealOutput.push(mealFoods[meal])
-      }
-      res.send(mealOutput)
-    })
+    Meal.mealsWithFoods()
+      .then((data) => {res.send(data)})
   }
 }
 
--- models/meal.js
+++ models/meal.js
@@ -5,7 +5,41 @@ const database = require('knex')(configuration)
 
 class Meal {
   all() {
-    return database.raw(`SELECT meals.id AS meal_id, meals.name AS meal_name, foods.id AS food_id, foods.name AS food_name, foods.calories AS food_calories FROM meals INNER JOIN meal_foods ON meals.id = meal_foods.meal_id INNER JOIN foods on foods.id = meal_foods.food_id`)
+    return database.raw(`
+      SELECT meals.id AS meal_id,
+             meals.name AS meal_name,
+             foods.id AS food_id,
+             foods.name AS food_name,
+             foods.calories AS food_calories
+             FROM meals
+      INNER JOIN meal_foods ON meals.id = meal_foods.meal_id
+      INNER JOIN foods on foods.id = meal_foods.food_id`)
+  }
+  mealsWithFoods() {
+    return this.all()
+      .then((data) => {
+        let rows = data.rows
+        let mealFoods = rows.reduce((result, row) => {
+          if (!result[row.meal_id]) {
+            result[row.meal_id] = {
+              id: row.meal_id,
+              name: row.meal_name,
+              foods: []}
+          }
+          let food = {
+            id: row.food_id,
+            name: row.food_name,
+            calories: row.food_calories
+          }
+          result[row.meal_id].foods.push(food)
+          return result
+        }, {})
+        let mealOutput = []
+        for (let meal in mealFoods) {
+          mealOutput.push(mealFoods[meal])
+        }
+        return mealOutput
+      })
   }
 }
 
