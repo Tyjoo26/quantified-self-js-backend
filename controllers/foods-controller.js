c0e7ab88feb58bbcbd067ef9d9a650e491a55727
maxscores <maxstackhouse@gmail.com>
2018-03-25 19:47:57 -0600

    Reorganize food controller

--- controllers/foods-controller.js
+++ controllers/foods-controller.js
@@ -1,30 +1,29 @@
 var Food = require('../models/food')
-
-let foods = new Food
+Food = new Food
 
 class FoodsController {
   index(req, res) {
-    foods.all()
+    Food.all()
       .then((data) => {res.send(data.rows)})
   }
 
   show(req, res) {
-    foods.find(req.params.id)
+    Food.find(req.params.id)
       .then((data) => {res.send(data.rows)})
   }
 
   create(req, res) {
-    foods.create(req.body.food)
+    Food.create(req.body.food)
       .then((data) => {res.send(data.rows)})
   }
 
   update(req, res) {
-    foods.update(req.params.id, req.body.food)
+    Food.update(req.params.id, req.body.food)
       .then((data) => {res.send(data.rows)})
   }
 
   destroy(req, res) {
-    foods.destroy(req.params.id)
+    Food.destroy(req.params.id)
       .then((data) => {res.send(data.rows)})
   }
 }
