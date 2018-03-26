dd6759af7e32a11915415af3190edacdc230bc94
maxscores <maxstackhouse@gmail.com>
2018-03-25 19:13:47 -0600

    Allow Cors

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use('/api/v1/foods', apiV1FoodsRouter);
 app.use('/api/v1/meals', apiV1MealsRouter);
 
